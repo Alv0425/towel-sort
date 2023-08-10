4951ab0c95b313146075d5818673311bee288d63
Alv0425 <vilejshikova@gmail.com>
2023-08-10 17:44:40 +0300

    feat: add towel sort algoritm ang check if no parameters passed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    //start towelsort task
-    return [];
+    //return empty array if no params passed
+    if (typeof(matrix) == 'undefined') {
+        return [];
+    }
+    //towel sort
+    for (let i = 1; i < matrix.length; i += 2) {
+        matrix[i].reverse();
+    }
+    //return sorted matrix
+    return [].concat(...matrix);
 }
